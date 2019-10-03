3e5ad7bdb0015d67293e9cec649e2c19a63b7dad
ChrisXtx <55004840+ChrisXtx@users.noreply.github.com>
2019-10-03 14:08:29 -0400

    Update Train_run.py

--- Train_run.py
+++ Train_run.py
@@ -1,25 +1,18 @@
-#!/usr/bin/env python3
-
-# core - Neural Network Toolkit
-#
-# Copyright (c) 2017 - now
-# Max Planck Institute of Neurobiology, Munich, Germany
-# Authors: Martin Drawitsch, Philipp Schubert
-
 import argparse
-import logging
-import os
+import time
 import random
 import zipfile
+from torch.utils.data import DataLoader
+from core.data.utils import *
 
 import torch
 from torch import nn
 from torch import optim
 import numpy as np
+from torch.autograd import Variable
+import torch.nn.functional as F
 
 parser = argparse.ArgumentParser(description='Train a network.')
-parser.add_argument('--disable-cuda', action='store_true', help='Disable CUDA')
-parser.add_argument('-n', '--exp-name', default=None, help='Manually set experiment name')
 parser.add_argument(
     '-s', '--epoch-size', type=int, default=100,
     help='How many training samples to process between '
@@ -39,15 +32,6 @@ parser.add_argument(
          'ScriptModule from which to resume training.'
 )
 parser.add_argument(
-    '-j', '--jit', metavar='MODE', default='onsave',
-    choices=['disabled', 'train', 'onsave'],
-    help="""Options:
-"disabled": Completely disable JIT tracing;
-"onsave": Use regular Python model for training, but trace it on-demand for saving training state;
-"train": Use traced model for training and serialize it on disk"""
-)
-parser.add_argument('--seed', type=int, default=0, help='Base seed for all RNGs.')
-parser.add_argument(
     '--deterministic', action='store_true',
     help='Run in fully deterministic mode (at the cost of execution speed).'
 )
@@ -56,11 +40,6 @@ parser.add_argument('-i', '--ipython', action='store_true',
 )
 args = parser.parse_args()
 
-# Set up all RNG seeds, set level of determinism
-random_seed = args.seed
-torch.manual_seed(random_seed)
-np.random.seed(random_seed)
-random.seed(random_seed)
 deterministic = args.deterministic
 if deterministic:
     torch.backends.cudnn.deterministic = True
@@ -72,89 +51,22 @@ import core
 core.select_mpl_backend('Agg')
 logger = logging.getLogger('elektronn3log')
 
-from core.data import PatchCreator, transforms, utils, get_preview_batch
-from core.training import Trainer, Backup, metrics
-from core.training import SWA
-from core.modules import DiceLoss, CombinedLoss
-from core.models.unet import UNet
+from core.training import metrics
 from core.models.ffn import FFN
+from core.data import BatchCreator
 
-
-if not args.disable_cuda and torch.cuda.is_available():
-    device = torch.device('cuda')
-else:
-    device = torch.device('cpu')
+device = torch.device('cuda')
 logger.info(f'Running on device: {device}')
 
 
 def run():
-    # model = UNet(
-    #     n_blocks=4,
-    #     start_filts=32,
-    #     planar_blocks=(0,),
-    #     activation='relu',
-    #     batch_norm=True,
-    #     # conv_mode='valid',
-    #     # up_mode='resizeconv_nearest',  # Enable to avoid checkerboard artifacts
-    #     adaptive=True  # Experimental. Disable if results look weird.
-    # ).to(device)
-    model = FFN(in_channels=1, out_channels=2).to(device)
-    # Example for a model-compatible input.
-    example_input = torch.ones(1, 1, 32, 64, 64)
+    model = FFN(in_channels=2, out_channels=1).to(device)
 
-    enable_save_trace = False if args.jit == 'disabled' else True
-    if args.jit == 'onsave':
-        # Make sure that tracing works
-        tracedmodel = torch.jit.trace(model, example_input.to(device))
-    elif args.jit == 'train':
-        if getattr(model, 'checkpointing', False):
-            raise NotImplementedError(
-                'Traced models with checkpointing currently don\'t '
-                'work, so either run with --disable-trace or disable '
-                'checkpointing.')
-        tracedmodel = torch.jit.trace(model, example_input.to(device))
-        model = tracedmodel
-
-
-    # USER PATHS
     save_root = os.path.expanduser('./log/ffn/')
     os.makedirs(save_root, exist_ok=True)
-    if os.getenv('CLUSTER') == 'WHOLEBRAIN':  # Use bigger, but private data set
-        data_root = '/wholebrain/scratch/j0126/barrier_gt_phil/'
-        fnames = sorted([f for f in os.listdir(data_root) if f.endswith('.h5')])
-        input_h5data = [(os.path.join(data_root, f), 'raW') for f in fnames]
-        target_h5data = [(os.path.join(data_root, f), 'labels') for f in fnames]
-        valid_indices = [1, 3, 5, 7]
 
-        # These statistics are computed from the training dataset.
-        # Remember to re-compute and change them when switching the dataset.
-        dataset_mean = (0.6170815,)
-        dataset_std = (0.15687169,)
-        # Class weights for imbalanced dataset
-        class_weights = torch.tensor([0.2808, 0.7192]).to(device)
-    else:  # Use publicly available neuro_data_cdhw dataset
-        data_root = os.path.expanduser('./data/')
-        input_h5data = [
-            (os.path.join(data_root, f'raw_{i}.h5'), 'raw')
-            for i in range(3)
-        ]
-        target_h5data = [
-            (os.path.join(data_root, f'barrier_int16_{i}.h5'), 'lab')
-            for i in range(3)
-        ]
-        valid_indices = [2]
+    input_h5data = ['./data.h5']
 
-        dataset_mean = (155.291411,)
-        dataset_std = (42.599973,)
-        class_weights = torch.tensor([0.2653, 0.7347]).to(device)
-
-    # TODO: Recalculate above class_weights with mode='inverse'
-
-    max_steps = args.max_steps
-    max_runtime = args.max_runtime
-
-    optimizer_state_dict = None
-    lr_sched_state_dict = None
     if args.resume is not None:  # Load pretrained network
         pretrained = os.path.expanduser(args.resume)
         _warning_str = 'Loading model without optimizer state. Prefer state dicts'
@@ -177,93 +89,16 @@ def run():
             else:
                 raise ValueError(f'Can\'t load {pretrained}.')
 
-    # Transformations to be applied to samples before feeding them to the network
-    common_transforms = [
-        transforms.SqueezeTarget(dim=0),  # Workaround for neuro_data_cdhw
-        transforms.Normalize(mean=dataset_mean, std=dataset_std)
-    ]
-    train_transform = transforms.Compose(common_transforms + [
-        # transforms.RandomRotate2d(prob=0.9),
-        # transforms.RandomGrayAugment(channels=[0], prob=0.3),
-        # transforms.RandomGammaCorrection(gamma_std=0.25, gamma_min=0.25, prob=0.3),
-        # transforms.AdditiveGaussianNoise(sigma=0.1, channels=[0], prob=0.3),
-    ])
-    valid_transform = transforms.Compose(common_transforms + [])
+    train_dataset = BatchCreator(input_h5data, (33, 33, 33), delta=(8, 8, 8), train=True)
+    train_loader = DataLoader(train_dataset, shuffle=True, num_workers=1, pin_memory=True)
 
-    # Specify data set
-    aniso_factor = 2  # Anisotropy in z dimension. E.g. 2 means half resolution in z dimension.
-    common_data_kwargs = {  # Common options for training and valid sets.
-        'aniso_factor': aniso_factor,
-        'patch_shape': (48, 96, 96),
-        # 'offset': (8, 20, 20),
-        'num_classes': 2,
-        # 'in_memory': True  # Uncomment to avoid disk I/O (if you have enough host memory for the data)
-    }
-    train_dataset = PatchCreator(
-        input_h5data=[input_h5data[i] for i in range(len(input_h5data)) if i not in valid_indices],
-        target_h5data=[target_h5data[i] for i in range(len(input_h5data)) if i not in valid_indices],
-        train=True,
-        epoch_size=args.epoch_size,
-        warp_prob=0.2,
-        warp_kwargs={
-            'sample_aniso': aniso_factor != 1,
-            'perspective': True,
-            'warp_amount': 0.1,
-        },
-        transform=train_transform,
-        **common_data_kwargs
-    )
-    valid_dataset = None if not valid_indices else PatchCreator(
-        input_h5data=[input_h5data[i] for i in range(len(input_h5data)) if i in valid_indices],
-        target_h5data=[target_h5data[i] for i in range(len(input_h5data)) if i in valid_indices],
-        train=False,
-        epoch_size=10,  # How many samples to use for each validation run
-        warp_prob=0,
-        warp_kwargs={'sample_aniso': aniso_factor != 1},
-        transform=valid_transform,
-        **common_data_kwargs
-    )
-
-    # Use first validation cube for previews. Can be set to any other data source.
-    preview_batch = get_preview_batch(
-        h5data=input_h5data[valid_indices[0]],
-        preview_shape=(32, 320, 320),
-        transform=transforms.Normalize(mean=dataset_mean, std=dataset_std)
-    )
-
-    optimizer = optim.SGD(
+    optimizer = optim.Adam(
         model.parameters(),
-        lr=0,  # Learning rate is set by the lr_sched below
-        momentum=0.9,
+        lr=1e-3,  # Learning rate is set by the lr_sched below
+        # momentum=0.9,
         weight_decay=0.5e-4,
     )
-    optimizer = SWA(optimizer)  # Enable support for Stochastic Weight Averaging
 
-    # Set to True to perform Cyclical LR range test instead of normal training
-    #  (see https://arxiv.org/abs/1506.01186, sec. 3.3).
-    do_lr_range_test = False
-    if do_lr_range_test:
-        # Begin with a very small lr and double it every 1000 steps.
-        for grp in optimizer.param_groups:
-            grp['lr'] = 1e-7  # Note: lr will be > 1.0 after 24k steps.
-        lr_sched = torch.optim.lr_scheduler.StepLR(optimizer, 1000, 2)
-    else:
-        lr_sched = torch.optim.lr_scheduler.CyclicLR(
-            optimizer,
-            base_lr=1e-6,
-            max_lr=1e-3,
-            step_size_up=10000,
-            step_size_down=10000,
-            cycle_momentum=True
-        )
-        if optimizer_state_dict is not None:
-            optimizer.load_state_dict(optimizer_state_dict)
-        if lr_sched_state_dict is not None:
-            lr_sched.load_state_dict(lr_sched_state_dict)
-
-    # All these metrics assume a binary classification problem. If you have
-    #  non-binary targets, remember to adapt the metrics!
-    # TODO: Default to mean metrics instead of binary versions?
     valid_metrics = {
         'val_accuracy': metrics.bin_accuracy,
         'val_precision': metrics.bin_precision,
@@ -272,54 +107,52 @@ def run():
         'val_IoU': metrics.bin_iou,
     }
 
+    for iter, (data, label, seed, coor)in enumerate(train_loader):
 
-    crossentropy = nn.CrossEntropyLoss(weight=class_weights)
-    dice = DiceLoss(apply_softmax=True, weight=class_weights)
-    criterion = CombinedLoss([crossentropy, dice], weight=[0.5, 0.5], device=device)
+        # while(1):
+            offsets = []
+            for idx, offset in enumerate(fixed_offsets(seed, train_dataset.shifts, 0.9)):
+                offsets.append(np.array(offset))
+                if idx == 3:##bach_size-1
+                    break
+            if len(offsets) == 0:
+                break
 
-    # Create trainer
-    trainer = Trainer(
-        model=model,
-        criterion=criterion,
-        optimizer=optimizer,
-        device=device,
-        train_dataset=train_dataset,
-        valid_dataset=valid_dataset,
-        batchsize=1,
-        num_workers=0,
-        save_root=save_root,
-        exp_name=args.exp_name,
-        example_input=example_input,
-        enable_save_trace=enable_save_trace,
-        schedulers={'lr': lr_sched},
-        valid_metrics=valid_metrics,
-        preview_batch=preview_batch,
-        preview_interval=5,
-        # enable_videos=True,  # Uncomment to enable videos in tensorboard
-        offset=train_dataset.offset,
-        apply_softmax_for_prediction=True,
-        num_classes=train_dataset.num_classes,
-        # TODO: Tune these:
-        preview_tile_shape=(32, 64, 64),
-        preview_overlap_shape=(32, 64, 64),
-        ipython_shell=args.ipython,
-        # mixed_precision=True,  # Enable to use Apex for mixed precision training
-    )
+            images = np.zeros([len(offsets)]+[1, 33, 33, 33])
+            labels = np.zeros([len(offsets)]+[1, 33, 33, 33])
+            seeds = np.zeros([len(offsets)]+[1, 33, 33, 33])
+            for idx, offset in enumerate(offsets):
+                start = offset + model.radii - model.input_size // 2
+                end = start + model.input_size
+                assert np.all(start >= 0)
 
-    if args.deterministic:
-        assert trainer.num_workers <= 1, 'num_workers > 1 introduces indeterministic behavior'
+                selector = [slice(s, e) for s, e in zip(start, end)]
+                images[idx][0] = data[0][selector]
+                labels[idx][0] = label[0][selector]
+                seeds[idx][0] = seed[0][selector]
 
-    # Archiving training script, src folder, env info
-    Backup(script_path=__file__,save_path=trainer.save_path).archive_backup()
+            images = torch.from_numpy(images).float()
+            labels = torch.from_numpy(labels).float()
+            seeds = torch.from_numpy(seeds).float()
+            input_data = torch.cat([images, seeds], dim=1)
 
-    # Start training
-    trainer.run(max_steps=max_steps, max_runtime=max_runtime)
+            input_data = Variable(input_data.cuda())
+            seeds = seeds.cuda()
+            labels = labels.cuda()
 
+            logits = model(input_data)
 
-# How to re-calculate mean, std and class_weights for other datasets:
-#  dataset_mean = utils.calculate_means(train_dataset.inputs)
-#  dataset_std = utils.calculate_stds(train_dataset.inputs)
-#  class_weights = torch.tensor(utils.calculate_class_weights(train_dataset.targets))
+            updated = seeds + logits
+            optimizer.zero_grad()
+            loss = F.binary_cross_entropy_with_logits(updated, labels)
+            loss.backward()
+            optimizer.step()
+            print("loss: {}, offset: {}".format(loss.item(), offsets))
+            update_seed(updated, seed, model, offsets)
+
 
 if __name__ == "__main__":
+    seed = int(time.time())
+    random.seed(seed)
+
     run()
