7c94f9330ce81216890a5c45e2bce99e8fde9804
ChrisXtx <55004840+ChrisXtx@users.noreply.github.com>
2019-10-06 14:15:31 -0400

    Update FFN_net.py

--- FFN_net.py
+++ FFN_net.py
@@ -1,5 +1,7 @@
+    
 __all__ = ['FFN']
 
+import numpy as np
 import torch.nn as nn
 import torch.nn.functional as F
 
@@ -18,13 +20,18 @@ class ResBlock(nn.Module):
 
 
 class FFN(nn.Module):
-    def __init__(self, in_channels=2, mid_channels=32, out_channels=2, kernel_size=(3, 3, 3), padding=1, depth=12):
+    def __init__(self, in_channels=2, mid_channels=32, out_channels=1, kernel_size=(3, 3, 3), padding=1, depth=12,
+                 input_size=[33, 33, 33], delta=[8, 8, 8]):
         super(FFN, self).__init__()
 
         self.conv0 = nn.Conv3d(in_channels, mid_channels, kernel_size, padding=padding)
         self.conv1 = nn.Conv3d(mid_channels, mid_channels, kernel_size, padding=padding)
         self.resblocks = nn.Sequential(*[ResBlock(mid_channels, mid_channels, kernel_size, padding) for i in range(depth)])
         self.conv3 = nn.Conv3d(mid_channels, out_channels, (1, 1, 1))
+
+        self.input_size = np.array(input_size)
+        self.delta = np.array(delta)
+        self.radii = (self.input_size + self.delta*2) // 2
 
         self._init_weights()
 
